5326f0b76e7c3a4947ef815998abbcb0fb57b316
mori-akira <77237845+mori-akira@users.noreply.github.com>
2024-02-12 22:36:52 +0900

    study

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+const hoge = '';
+const fuga = hoge;
+console.log(fuga);
+const hoge2 = 2;
+const fuga2 = hoge;
+console.log(fuga2);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+
+const hoge: any = '';
+const fuga = hoge as string;
+console.log(fuga);
+
+const hoge2: any = 2;
+const fuga2 = hoge as string;
+console.log(fuga2);
