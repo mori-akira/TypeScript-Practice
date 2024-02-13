b8584899d69c0c960ad0334221bce1e9ab4b362d
mori-akira <77237845+mori-akira@users.noreply.github.com>
2024-02-13 23:11:46 +0900

    Update

--- dist/index.js
+++ dist/index.js
@@ -5,4 +5,17 @@ console.log(fuga);
 const hoge2 = 2;
 const fuga2 = hoge;
 console.log(fuga2);
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 4] = "Red";
+    Color[Color["Blue"] = 1] = "Blue";
+    Color[Color["Green"] = 2] = "Green";
+})(Color || (Color = {}));
+const c1 = Color.Red;
+const obj = {
+    name: 'Sato',
+    age: 32,
+    subject: 'Tokyo',
+};
+console.log(obj.length);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-
 const hoge: any = '';
 const fuga = hoge as string;
 console.log(fuga);
@@ -6,3 +5,52 @@ console.log(fuga);
 const hoge2: any = 2;
 const fuga2 = hoge as string;
 console.log(fuga2);
+
+enum Color {
+    Red = 0b100,
+    Blue = 0b001,
+    Green = 0b010,
+}
+const c1: Color = Color.Red;
+// const c2: Color = Color.Purple;
+
+// 型エイリアス
+type str = string;
+type user = {
+    name: string,
+    age: number,
+    type: 1 | 2 | 3 | 4,
+}
+
+const obj: any = {
+    name: 'Sato',
+    age: 32,
+    subject: 'Tokyo',
+}
+console.log((obj as string).length);
+
+type Data = Record<string, Record<string, number | string>>;
+const data: Data = {
+    "hoge": {
+        "fuga": 1,
+    },
+    "piyo": {
+        "foo": "",
+    }
+}
+
+type KeyValue = {
+    [key in string]: string;
+}
+const kv: KeyValue = {
+    "a": "b",
+    "c": "d",
+}
+
+type MixedArray = (string | number | undefined)[];
+type T = MixedArray[number];
+
+const t: T = 1;
+
+type IsString<T> = T extends string ? true : false;
+const isstr: IsString<"a"> = true;
